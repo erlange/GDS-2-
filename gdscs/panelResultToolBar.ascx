3a941d728869911727a7db9d2ebb0560ceb669a7
erlange <eri.airlangga@gmail.com>
2020-02-15 03:10:21 +0700

    Analysis Type 1 Commit

---
+++ gdscs/panelResultToolBar.ascx
@@ -0,0 +1,5 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="panelResultToolBar.ascx.cs" Inherits="gds.panelResultToolBar" %>
+<a href="#varsel"><img border="0" src="images/tree.gif"><asp:label id="lblVarSel" runat="server">Pilih Variabel Lain</asp:label></a>&nbsp;&nbsp;
+<asp:linkbutton id="btnExcel" runat="server" causesvalidation="False" 
+    tooltip="Download Excel File" onclick="btnExcel_Click"><img src="imgedit/xls.gif" border="0" />Download Excel</asp:linkbutton>&nbsp;&nbsp;
+<a id="btnPrint2" runat="server" href="javascript:void(null);" onmouseover="window.status='Print'" onmouseout="window.status=''"><img border="0" src="imgedit/print.gif"><asp:label id="Label1" runat="server">Print</asp:label></a>&nbsp;&nbsp;
---
+++ gdscs/panelResultToolBar.ascx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace gds
+{
+    public partial class panelResultToolBar : System.Web.UI.UserControl
+    {
+        public event btnExcelClickEventHandler btnExcelClick;
+        public delegate void btnExcelClickEventHandler(object sender, EventArgs e);
+
+        public event btnPrintClickEventHandler btnPrintClick;
+        public delegate void btnPrintClickEventHandler(object sender, EventArgs e);
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (commonModule.IsEnglish())
+            {
+                this.lblVarSel.Text = "Select Other Variable";
+            }
+            else
+            {
+                this.lblVarSel.Text = "Pilih Variabel Lain";
+            }
+        }
+        public void SetPrintUrl(string url)
+        {
+            btnPrint2.Attributes.Add("onclick", url);
+
+        }
+
+        protected void btnExcel_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+
+    }
+}
